b5b4e46a2558df7ab53d5d47f7547d2308348395
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-02-22 21:21:39 +0530

    description about code added to get understand

--- 13.page-scroll-indicator/app.js
+++ 13.page-scroll-indicator/app.js
@@ -1,12 +1,12 @@
 let indicator = document.querySelector(".scroll-indicator .scroll-filler");
 
+//Down below we have overAll scrollHeight from  document.documentElement.scrollHeight and we subtract document.documentElement.clientHeight to get scrollable height
 const scrollHeight =
   document.documentElement.scrollHeight - document.documentElement.clientHeight;
-window.addEventListener("scroll", scroll);
 
 function scroll() {
+  //From below scrollTop we get how much we scroll which further divided by total scrollable height  until they  became same and convert it into percentage
   let scrollTop = document.documentElement.scrollTop;
   let totalHeight = (scrollTop / scrollHeight) * 100;
   indicator.style.width = `${totalHeight}%`;
-  console.log(totalHeight);
 }
